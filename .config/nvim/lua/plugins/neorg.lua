a72a150ef6a641a00d27cec079338910a5e043f5
sketchsoh <sketchsoh@gmail.com>
2024-04-10 11:16:18 +0800

    Config

--- .config/nvim/lua/plugins/neorg.lua
+++ .config/nvim/lua/plugins/neorg.lua
@@ -4,6 +4,13 @@ return {
     dependencies = { "luarocks.nvim" },
     lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
     version = "*", -- Pin Neorg to the latest stable release
+    keys = {
+      { "<leader>ni", "<cmd>Neorg index<cr>", desc = "Neorg Index" },
+      { "<leader>nw", "<cmd>Neorg workspace<cr>", desc = "Neorg Workspace" },
+      { "<leader>nj", "<cmd>Neorg journal<cr>", desc = "Neorg Journal" },
+      { "<leader>nm", "<cmd>Neorg inject-metadata<cr>", desc = "Neorg inject metadata" },
+      { "<leader>nn", "<cmd>Neorg keybind norg core.dirman.new.note<cr>", desc = "Neorg new note" },
+    },
     config = function()
       require("neorg").setup({
         load = {
@@ -31,5 +38,8 @@ return {
 
   {
     "dhruvasagar/vim-table-mode",
+    keys = {
+      { "<leader>tmd", "<cmd>TableModeToggle<cr>", desc = "Toggle Table Mode" },
+    },
   },
 }
